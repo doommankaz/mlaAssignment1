7ecdbc738b442d464a69b334852c271f17340be3
Duman <doommankaz@gmail.com>
2022-11-01 12:05:35 +0600

    clusterization

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import re
 import nltk
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.cluster
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.corpus import wordnet
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from math import sqrt, floor
+from sklearn.cluster import KMeans
 
 
 # nltk.download('wordnet')
@@ -71,6 +76,7 @@ def computetfidf(wordarray):
     # convert th documents into a matrix
     count_wm = countvectorizer.fit_transform(wordarray)
     tfidf_wm = tfidfvectorizer.fit_transform(wordarray)
+    print(tfidf_wm.toarray())
 
     count_tokens = countvectorizer.get_feature_names_out()
     tfidf_tokens = tfidfvectorizer.get_feature_names_out()
@@ -82,6 +88,56 @@ def computetfidf(wordarray):
     print(df_countvect)
     print("\nTD-IDF Vectorizer\n")
     print(df_tfidfvect)
+    print(naive_sharding(tfidf_wm.toarray(), 3))
+    # clusterization(df_tfidfvect)
+
+
+def clusterization(df_tfidfvect):
+    kmeans = sklearn.cluster.KMeans(n_clusters=3, init='k-means++', random_state=0).fit(df_tfidfvect)
+    print(kmeans.cluster_centers_)
+
+    # inertias = []
+    #
+    # for i in range(1, 11):
+    #     kmeans = KMeans(n_clusters=i)
+    #     kmeans.fit(tfidf_wm.toarray())
+    #     inertias.append(kmeans.inertia_)
+    #
+    # plt.plot(range(1, 11), inertias, marker='o')
+    # plt.title('Elbow method')
+    # plt.xlabel('Number of clusters')
+    # plt.ylabel('Inertia')
+    # plt.show()
+    #
+    # kmeans = KMeans(n_clusters=2)
+    # kmeans.fit(tfidf_wm.toarray())
+    #
+    # plt.scatter(tfidf_wm.toarray(), c=kmeans.labels_)
+    # plt.show()
+
+
+def naive_sharding(ds, k):
+    def _get_mean(sums, step):
+        return sums / step
+
+    n = np.shape(ds)[1]
+    m = np.shape(ds)[0]
+    centroids = np.zeros((k, n))
+
+    composite = np.mat(np.sum(ds, axis=1))
+    ds = np.append(composite.T, ds, axis=1)
+    ds.sort(axis=0)
+
+    step = floor(m / k)
+    vfunc = np.vectorize(_get_mean)
+
+    for j in range(k):
+        if j == k - 1:
+            centroids[j:] = vfunc(np.sum(ds[j * step:, 1:], axis=0), step)
+        else:
+            centroids[j:] = vfunc(np.sum(ds[j * step:(j + 1) * step, 1:], axis=0), step)
+
+    return centroids
 
 
 if __name__ == '__main__':
